f13ae7b65cc01e0001b316b10419114233c3d833
thiagofarbo <thiagofarbo@gmail.com>
2019-01-06 12:43:32 -0200

    Ajustes

--- database/databaseConnection.ts
+++ database/databaseConnection.ts
@@ -1,15 +1,15 @@
 import * as mongoose from 'mongoose';
-
+ 
 class Database{
-
+ 
     private URL = 'mongodb://link-db/newsDatabase';
-
+ 
     createConnection(){
-
+ 
         mongoose.connect(this.URL);
-
+ 
     }
-
+ 
 }
-
+ 
 export default Database;
