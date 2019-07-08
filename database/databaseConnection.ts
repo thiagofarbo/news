8d7d17a572592747e7e2a72849f44be1043bb070
Thiago Emidio <thiagofarbo@gmail.com>
2019-07-08 16:41:27 +0100

    Added cors to our project

--- database/databaseConnection.ts
+++ database/databaseConnection.ts
@@ -1,8 +1,8 @@
 import * as mongoose from 'mongoose';
  
 class Database{
- 
-    private URL = 'mongodb://link-db/newsDatabase';
+    private URL = 'mongodb://localhost:27017/newsDatabase';
+    // private URL = 'mongodb://link-db/newsDatabase';
  
     createConnection(){
  
--- dist/database/databaseConnection.js
+++ dist/database/databaseConnection.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 class Database {
     constructor() {
-        this.URL = 'mongodb://link-db/newsDatabase';
+        this.URL = 'mongodb://localhost:27017/newsDatabase';
     }
+    // private URL = 'mongodb://link-db/newsDatabase';
     createConnection() {
         mongoose.connect(this.URL);
     }
